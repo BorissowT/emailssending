a87c4ec5c67434a27c6b546365a850c95e788107
tbs99 <timsregisters@gmail.com>
2020-02-04 15:52:51 +0300

    initial commit

--- Send.py
+++ Send.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_mail import Mail, Message
 import os
 app = Flask(__name__)
+################## v1 ########
 
 class Config:
     """
@@ -16,8 +17,22 @@ class Config:
     # Используем переменные окружения для инициализации логина и пароля
     MAIL_USERNAME = 'timoshaborisov@yandex.ru'
     MAIL_PASSWORD = ''
+
+#################### v2 ##########################
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
+}
+###################################
+
 app.config.from_object(Config)
+#v2 app.config.update(mail_settings)
 mail = Mail(app)
+
 
 if __name__ == '__main__':
     with app.app_context():
